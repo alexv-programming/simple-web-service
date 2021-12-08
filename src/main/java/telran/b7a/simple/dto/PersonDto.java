79249a7458a2f055eecb2ed046f88e8c5297ac8b
Alexv <Alexv@LAPTOP-RMNSP2M9.mshome.net>
2021-12-08 11:41:30 +0200

    new project

---
+++ src/main/java/telran/b7a/simple/dto/PersonDto.java
@@ -0,0 +1,11 @@
+package telran.b7a.simple.dto;
+
+import lombok.Getter;
+
+@Getter
+
+public class PersonDto {
+	String firstName;
+	String lastName;
+
+}
---
+++ src/main/java/telran/b7a/simple/dto/PersonFoodDto.java
@@ -0,0 +1,20 @@
+package telran.b7a.simple.dto;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Singular;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PersonFoodDto {
+	String fullName;
+	@Singular //Сингулярит 
+	List<String> foods;
+
+}
